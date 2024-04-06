949e35755a925a560cd1a449f2a739672ff9a3c9
Peam <peammy2010@hotmail.com>
2024-04-06 16:51:02 +0700

    feat: booking

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,42 @@ import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { JWTPlugin, authPlugin } from "./feature/auth/plugin";
 import { authRouter } from "./feature/auth/router";
+import { bookingRouter } from "./feature/booking/router";
 import { campgroundsRouter } from "./feature/campground/router";
 
-const app = new Elysia()
-    .use(
-        swagger({
-            provider: "swagger-ui",
-            documentation: {
-                tags: [
-                    { name: "Campground", description: "Campground's endpoints" },
-                    { name: "Auth", description: "Authentication endpoints" },
-                    { name: "Booking", description: "Booking endpoints" },
-                ],
-            },
-        })
-    )
-    .use(JWTPlugin)
-    .use(authPlugin)
-    .use(authRouter)
-    .use(campgroundsRouter)
-    .get(
-        "/",
-        ({ user }) => {
-            return {
-                message: `Hello, ${user?.name}`,
-            };
-        },
-        {
-            auth: ["user"],
-        }
-    )
-    .listen(3000);
+export const app = new Elysia()
+  .use(
+    swagger({
+      provider: "swagger-ui",
+      documentation: {
+        tags: [
+          { name: "Campground", description: "Campground's endpoints" },
+          { name: "Auth", description: "Authentication endpoints" },
+          { name: "Booking", description: "Booking endpoints" },
+        ],
+      },
+    }),
+  )
+  .use(JWTPlugin)
+  .use(authPlugin)
+  .use(authRouter)
+  .use(campgroundsRouter)
+  .use(bookingRouter)
+  .get(
+    "/",
+    ({ user }) => {
+      return {
+        message: `Hello, ${user?.name}`,
+      };
+    },
+    {
+      auth: ["user"],
+    },
+  )
+  .listen(3000);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
 
 export type AppType = typeof app;
